86d91d6203feb70e638aaec02b188d7281a1e99d
otrab <otrab86@gmail.com>
2024-10-06 17:25:30 -0300

    semaphore-producerconsumer

---
+++ C7/semaphore-producerconsumer/main.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+#define MAX 50
+
+int buffer[MAX];
+int fill = 0;
+int use = 0;
+int count = 0;
+
+sem_t data, empty, mutex;
+
+void put(int value) {
+  buffer[fill] = value;
+  fill = (fill + 1) % MAX;
+  count++;
+}
+
+int get() {
+  int tmp = buffer[use];
+  use = (use + 1) % MAX;
+  count--;
+  return tmp;
+}
+
+void *producer(void *arg) {
+  int i;
+  int loops = (int) arg;
+  for (i = 0; i < loops; i++) {
+    sem_wait(&empty);
+    sem_wait(&mutex);
+    put(i);
+    sem_post(&data);
+    sem_post(&mutex);
+  }
+  return NULL;
+}
+
+void *consumer(void *arg) {
+  int i;
+  int loops = (int) arg;
+  for (i = 0; i < loops; i++) {
+    sem_wait(&data);
+    sem_wait(&mutex);
+    int tmp = get();
+    printf("%d\n", tmp);
+    sem_post(&empty);
+    sem_post(&mutex);
+  }
+  return NULL;
+}
+
+int main(void) {
+  sem_init(&data, 0, 0);
+  sem_init(&empty, 0, MAX);
+  sem_init(&mutex, 0, 1);
+
+  pthread_t p1, p2;
+  pthread_create(&p1, NULL, producer, (void*)(MAX*2));
+  pthread_create(&p2, NULL, consumer, (void*)MAX);
+
+  pthread_join(p1, NULL);
+  pthread_join(p2, NULL);
+  
+  printf("listo!\n");
+
+  return 0;
+}
